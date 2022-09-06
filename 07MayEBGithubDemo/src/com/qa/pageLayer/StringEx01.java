ddc72b8f2b57c077bed85db7a4fda9ec18700dee
kishorwadkar <kishorwadkar1717@gmail.com>
2022-09-07 00:14:09 +0530

    StringEx01
    i have created StringEx01 in our projecct

---
+++ 07MayEBGithubDemo/src/com/qa/pageLayer/StringEx01.java
@@ -0,0 +1,17 @@
+package com.qa.pageLayer;
+
+public class StringEx01 
+{
+	public static void main(String[] args)
+	{
+		
+		
+	
+	String s="Welcome To Java";
+	String a=s.replaceAll(" ", "");
+	System.out.println(a);
+	
+	
+	}
+
+}
